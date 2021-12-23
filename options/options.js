a42281ddad19c8c9db9bb1cdd7f4bc3193db753a
Crowdbotics <team@crowdbotics.com>
2021-12-23 15:01:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "leupitz_inspections_32562",
+  url: "https://leupitz_inspections_32562.botics.co",
+  api: "https://leupitz_inspections_32562.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
